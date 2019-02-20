945559f8af9dd13cb90a69ef246632fade5b4cd5
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2019-02-20 11:13:08 +0800

    从其它服务器下载过来解压到目标目录

--- getcertrenew.sh
+++ getcertrenew.sh
@@ -1,6 +1,24 @@
 #!bash
 #Auth: ysun
 
-wget -N ysun.cn/cer.zip
-/usr/bin/unzip -o /root/cer.zip -d /root/
-systemctl restart httpd
+#source zip: cbb.abc.com/path/cer.zip
+mdomain=某某.com/path
+certzipfile=cer.zip
+
+#dir
+dirpath=/root/
+cd $dirpath
+
+[ "${mdomain:0-1}" == "/" ] && webzip=$mdomain$certzipfile || webzip=$mdomain/$certzipfile
+wget -N $webzip
+
+if [ -s "$certzipfile" ]; then
+    fd=`ls --full-time $certzipfile | awk '{print $6}'`
+    nd=`date +'%Y-%m-%d'`
+
+    if [ "$nd" == "$fd" ]; then
+        [ "${dirpath:0-1}" == "/" ] && dirpath=$dirpath || dirpath=$dirpath/
+        /usr/bin/unzip -o $dirpath$certzipfile -d $dirpath
+        systemctl restart httpd
+    fi
+fi
