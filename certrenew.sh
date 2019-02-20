7a77614921e1d53e700b2a16abec43a16bfb41c2
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2019-02-20 10:43:32 +0800

    打个包放网站目录，让其它服务器可以下载（不安全）。

--- certrenew.sh
+++ certrenew.sh
@@ -1,22 +1,46 @@
 #!bash
 #Auth: ysun
 
+#main domain: the folder name in /etc/letsencrypt/live/
+mdomain=某某.com
+#zip path to web
+certzipfile=/var/www/html/cer.zip
+
 #renew log file name
 d1="/root/renewlog/"
 tname=`date +${d1}%Y%m%d-%H%M%S.log`
 date >${tname}
+
+#rm logfile > 3 month
+cd $d1
+for a in $(ls)
+do
+    f=${a%-*}
+    n=`date +%Y%m%d`
+    #echo $f,$n
+    s=$((n-f));
+    [ $s -gt 300 ] && rm -f $a >>${tname}
+done
+
 #run renew everyday, certbot will check, if less 30 days then do renew
 /usr/bin/certbot renew >>${tname}
 
-#make a pfx for IIS,(password:1234)
-#cd /etc/letsencrypt/live/baidu.com
-#openssl pkcs12 -export -out a.pfx -inkey privkey.pem -in cert.pem -passout pass:1234
+#get the date of pem
+cd /etc/letsencrypt/live/$mdomain
+fd=`ls --full-time cert.pem | awk '{print $6}'`
+nd=`date +'%Y-%m-%d'`
 
-#delete old zipfile
-rm -f /var/www/html/cer.zip
-#create new zipfile
-cd /etc/letsencrypt/live
-/usr/bin/zip -r /var/www/html/cer.zip baidu.com
+#check if the pem renewed today
+if [ "$nd" == "$fd" ]; then
+    #make a pfx for IIS (password:1234)
+    #openssl pkcs12 -export -out a.pfx -inkey privkey.pem -in cert.pem -passout pass:1234
 
-#restart the http/https
-/bin/systemctl restart httpd
+    #delete old zipfile
+    rm -f $certzipfile
+    #create new zipfile
+    cd /etc/letsencrypt/live
+    /usr/bin/zip -r $certzipfile $mdomain
+
+    #restart the http/https
+    /bin/systemctl restart httpd
+fi
